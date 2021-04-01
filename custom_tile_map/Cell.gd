6c52f56a8a505a1867d464e3cf31ce1e4a410ed2
cobrapitz <12397702+cobrapitz@users.noreply.github.com>
2021-04-02 01:12:00 +0200

    changed cell data (added texturename)

--- custom_tile_map/Cell.gd
+++ custom_tile_map/Cell.gd
@@ -2,10 +2,18 @@ extends Resource
 
 
 export(bool) var visible
+export(Vector2) var size # unsues probably
 export(Vector2) var position
-export(Vector2) var size
-export(Rect2) var region_rect
-export(Texture) var texture
 export(Vector2) var offset
+
+export(Texture) var texture
+
+export(Array) var polygon
+
+export(Rect2) var texture_region_rect
+
+# for save/load
+export(String) var texture_name
+
 
 
